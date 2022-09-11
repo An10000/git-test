febaa7380be73054799a3c1dce7e336d59108847
An10000 <tigershan357@gmail.com>
2022-09-11 15:19:51 -0400

    second commit

--- src/git_test_package/Main.java
+++ src/git_test_package/Main.java
@@ -1,5 +1,9 @@
 package git_test_package;
 
-public class Main {
+import java.util.Arrays;
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
 }
