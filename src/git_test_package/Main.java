6442439acf67261283912ecd2583c338c2a72615
Andy Han <tigershan357@gmail.com>
2022-09-12 10:10:28 -0400

    third commit change to .gitignore

--- src/git_test_package/Main.java
+++ src/git_test_package/Main.java
@@ -1,10 +1,9 @@
 package git_test_package;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("hello git 1");
+        System.out.println("test push");
     }
 }
